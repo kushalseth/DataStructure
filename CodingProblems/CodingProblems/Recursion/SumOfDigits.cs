f542e5e288671cb1e3bca1e58636fd90d1fa7892
Kushal GIT <kushalseth1408@gmail.com>
2020-05-29 01:36:54 +0530

    Added SumOfDigits

---
+++ CodingProblems/CodingProblems/Recursion/SumOfDigits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.Recursion
+{
+    public static class SumOfDigits
+    {
+        public static int Sum(int n)
+          {
+            if (n == 0)
+                return 0;
+
+            return n % 10 + Sum(n / 10);
+
+        }
+    }
+}
