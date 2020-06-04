17c9c52d6ab4bf5d9efd8c21755715d68217fcf5
Kushal GIT <kushalseth1408@gmail.com>
2020-06-05 03:41:45 +0530

    array

---
+++ CodingProblems/CodingProblems/ArrayProblems/ArrayInsertAtEnd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.ArrayProblems
+{
+    public static class ArrayInsertAtEnd
+    {
+        public static int[] insertAtEnd(int[] arr, int sizeOfArray, int element)
+        {
+            Array.Resize(ref arr, sizeOfArray);
+            // equilvaent of java array = Arrays.copyOf(array, array.length + 1);
+
+            arr[sizeOfArray - 1] = element;
+            return arr;
+            
+
+        }
+    }
+}
--- CodingProblems/CodingProblems/Program.cs
+++ CodingProblems/CodingProblems/Program.cs
@@ -1,4 +1,5 @@
-﻿using CodingProblems.Recursion;
+﻿using CodingProblems.ArrayProblems;
+using CodingProblems.Recursion;
 
 namespace CodingProblems.Console
 {
@@ -12,8 +13,11 @@ namespace CodingProblems.Console
             //int sumofdigits = SumOfDigits.Sum(7530);
             //int countDigits = CountDigits.CountDigitsInNumber(530);
             //int digitalRoot = DigitalRoot.GetDigitalRoot(9999);
-            long nthFibonacciNumber = Fibonacci.GetNthFibonacciNumber(20);
+            //long nthFibonacciNumber = Fibonacci.GetNthFibonacciNumber(20);
             #endregion Recursion
+
+            int[] theArray = { 1, 3, 5, 7, 9 };
+            ArrayInsertAtEnd.insertAtEnd(theArray, 6, 90);
         }
     }
 }
