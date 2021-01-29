566d0add11cf75642e23db70b5aac64c9870b792
Kushal GIT <kushalseth1408@gmail.com>
2021-01-30 03:58:06 +0530

    rotate and array

--- CodingProblems/CodingProblems/ArrayProblems/RemoveDuplicatesFromSortedArray.cs
+++ CodingProblems/CodingProblems/ArrayProblems/RemoveDuplicatesFromSortedArray.cs
@@ -10,7 +10,16 @@ namespace CodingProblems.ArrayProblems
     {
         public static int[] RemoveDuplicates(int[] array)
         {
+            int capturedIndex = 1;
 
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] != array[i])
+                {
+                    array[capturedIndex] = array[i];
+                    capturedIndex++;
+                }
+            }
             return array;
         }
     }
