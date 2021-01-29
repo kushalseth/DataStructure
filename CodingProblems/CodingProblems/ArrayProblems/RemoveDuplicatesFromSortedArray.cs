7bd826154e688eb66ec836b6ee32e58fcec3a46d
Kushal GIT <kushalseth1408@gmail.com>
2021-01-30 03:56:57 +0530

    remove duplicates using c#

---
+++ CodingProblems/CodingProblems/ArrayProblems/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.ArrayProblems
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public static int[] RemoveDuplicates(int[] array)
+        {
+
+            return array;
+        }
+    }
+}
--- CodingProblems/CodingProblems/Program.cs
+++ CodingProblems/CodingProblems/Program.cs
@@ -17,11 +17,11 @@ namespace CodingProblems.Console
             int[] testCase1 = { 12, 9, 90, 80 };
             int[] testCase2 = { -10, -4, -2, -33, -12 };
             int[] testCase3 = { 9 };
-            int[] testCase4 = { 10, 20, 30 };
+            int[] testCase4 = { 20, 30, 30, 30, 40, 40, 40 };
 
 
             int[] input = (int[])testCase4.Clone(); ; 
-            int[] output = ReverseArray.ReversedArray((int[])input.Clone());
+            int[] output = RemoveDuplicatesFromSortedArray.RemoveDuplicates((int[])input.Clone());
 
             System.Console.WriteLine("Input:");
             System.Console.Write(string.Join(Environment.NewLine, input));
