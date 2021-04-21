6f51ae52d031cba9df73f540b8a8404ee8f30133
Kushal GIT <kushalseth1408@gmail.com>
2021-04-22 01:20:34 +0530

    insertion sort

--- javadatastructure/src/SortCommon.java
+++ javadatastructure/src/SortCommon.java
@@ -3,6 +3,21 @@ import java.util.Comparator;
 
 public class SortCommon {
 
+    public static int[] InsertionSort(int[] arr) {
+
+        for(int i = 0; i < arr.length; i++) {
+                int key = arr[i];
+                int j = i -1;
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+        }
+
+        return arr;
+    }
+
     public static int[] SelectionSort(int[] arr) {
 
         int min_index = 0;
