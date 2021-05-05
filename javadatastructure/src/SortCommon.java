9c22ce0618b2670de633677845543c70784d518d
Kushal GIT <kushalseth1408@gmail.com>
2021-05-06 01:40:21 +0530

    union using merge sort

--- javadatastructure/src/SortCommon.java
+++ javadatastructure/src/SortCommon.java
@@ -3,6 +3,21 @@ import java.util.Comparator;
 
 public class SortCommon {
 
+    public static void UniunUsingMergeSort(int a[], int b[], int m, int n) {
+
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(i>0 && a[i-1]==a[i]){i++;continue;}
+            if(j>0 && b[j-1]==b[j]){j++;continue;}
+            if(a[i]<b[j]){System.out.print(a[i]+" ");i++;}
+            else if(a[i]>b[j]){System.out.print(b[j]+" ");j++;}
+            else{System.out.print(a[i]+" ");i++;j++;}
+        }
+        while(i<m){if(i==0||a[i]!=a[i-1])System.out.print(a[i]+" ");i++;}
+        while(j<n){if(j==0||b[j]!=b[j-1])System.out.print(b[j]+" ");j++;}
+    }
+
+
     public static void IntersectionOfArray(int arr1[], int arr2[], int m, int n) {
         int i = 0; int j = 0;
         while(i < m && j < n) {
