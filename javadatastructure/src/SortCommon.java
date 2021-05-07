2a113d064cddcae5af9be106d9b83b0a1034c0c6
Kushal GIT <kushalseth1408@gmail.com>
2021-05-08 03:56:12 +0530

    merge sort and quick sort

--- javadatastructure/src/SortCommon.java
+++ javadatastructure/src/SortCommon.java
@@ -3,6 +3,116 @@ import java.util.Comparator;
 
 public class SortCommon {
 
+    static void qSortusingHoare(int arr[],int l,int h){
+        if(l<h){
+            int p=HoareMergePartition(arr,l,h);
+            qSort(arr,l,p);
+            qSort(arr,p+1,h);
+        }
+    }
+
+    static void qSort(int arr[],int l,int h){
+        if(l<h){
+            int p=iPartition(arr,l,h);
+            qSort(arr,l,p-1);
+            qSort(arr,p+1,h);
+        }
+    }
+
+    static int HoareMergePartition(int arr[], int l, int h) {
+
+            int pivot=arr[l];
+            int i=l-1,j=h+1;
+            while(true){
+                do{
+                    i++;
+                }while(arr[i]<pivot);
+                do{
+                    j--;
+                }while(arr[j]>pivot);
+                if(i>=j)return j;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+
+    }
+
+    static int iPartition(int arr[], int l, int h)
+    {
+        int pivot=arr[h];
+        int i=l-1;
+        for(int j=l;j<=h-1;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+        return i+1;
+    }
+
+    static void partition(int arr[], int l, int h, int p)
+    {
+        int[] temp=new int[h-l+1];int index=0;
+        for(int i=l;i<=h;i++)
+            if(arr[i]<=arr[p])
+            {
+                temp[index]=arr[i];index++;
+            }
+        for(int i=l;i<=h;i++)
+            if(arr[i]>arr[p])
+            {
+                temp[index]=arr[i];index++;
+            }
+        for(int i=l;i<=h;i++)
+            arr[i]=temp[i-l];
+    }
+
+    static int countInv(int arr[], int l, int r)
+    {
+        int res = 0;
+        if (l<r) {
+
+            int m = (r + l) / 2;
+
+            res += countInv(arr, l, m);
+            res += countInv(arr, m + 1, r);
+            res += countAndMerge(arr, l, m , r);
+        }
+        return res;
+    }
+
+    static int countAndMerge(int arr[], int l, int m, int r)
+    {
+        int n1=m-l+1, n2=r-m;
+        int[] left=new int[n1];int[] right=new int[n2];
+        for(int i=0;i<n1;i++)
+            left[i]=arr[i+l];
+        for(int j=0;j<n2;j++)
+            right[j]=arr[m+1+j];
+        int res=0,i=0,j=0,k=l;
+        while(i<n1 && j<n2){
+            if(left[i]<=right[j])
+            {arr[k++]=left[i++];}
+            else{
+                arr[k++]=right[j++];
+                res=res+(n1-i);
+            }
+        }
+        while(i<n1)
+            arr[k++]=left[i++];
+        while(j<n2)
+            arr[k++]=right[j++];
+        return res;
+    }
+
+
+
     public static void UniunUsingMergeSort(int a[], int b[], int m, int n) {
 
         int i=0,j=0;
