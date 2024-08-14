79b2f58a9f5c9f4939d051f4f30eb1d2dae0422f
Kushal Seth <kushalseth1408@gmail.com>
2024-08-14 05:12:44 +0200

    count digits

--- CodingProblems/CodingProblems/Program.cs
+++ CodingProblems/CodingProblems/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CodingProblems.ArrayProblems;
 using CodingProblems.Recursion;
+using CodingProblems.takeuforward;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +11,31 @@ namespace CodingProblems.Console
     {
         static void Main(string[] args)
         {
-            int[] testCase1 = { 2, 3, 0, 0, 4, 7, 0, 8 };
-            int[] testCase6 = { 0, 3, 0, 0, 4, 7, 0, 0 };
-            int[] testCase2 = { -42, 0, -22, 0, -12, -12, -3, -1 };
-            int[] testCase3 = { 9 };
 
-            int[] testCase4 = { 0, 9 };
+            int result = CountDigitsInNumber.CountDigits(1);
+            System.Console.WriteLine("result is " + result);
 
-            int[] testCase5 = { 5, 7, 10, 12, 12, 15, 15, 17 };
+            //int[] testCase1 = { 2, 3, 0, 0, 4, 7, 0, 8 };
+            //int[] testCase6 = { 0, 3, 0, 0, 4, 7, 0, 0 };
+            //int[] testCase2 = { -42, 0, -22, 0, -12, -12, -3, -1 };
+            //int[] testCase3 = { 9 };
+
+            //int[] testCase4 = { 0, 9 };
+
+            //int[] testCase5 = { 5, 7, 10, 12, 12, 15, 15, 17 };
 
 
-            int[] input = (int[])testCase5.Clone(); 
-            int[] output = MoveZerosToEndOfArray.MoveZerosToEnd((int[])input.Clone());
+            //int[] input = (int[])testCase5.Clone(); 
+            //int[] output = MoveZerosToEndOfArray.MoveZerosToEnd((int[])input.Clone());
 
-            System.Console.WriteLine("Input:");
-            System.Console.Write(string.Join(Environment.NewLine, input));
+            //System.Console.WriteLine("Input:");
+            //System.Console.Write(string.Join(Environment.NewLine, input));
 
-            System.Console.WriteLine("");
-            System.Console.WriteLine("Output:");
-            System.Console.Write(string.Join(Environment.NewLine, output));
+            //System.Console.WriteLine("");
+            //System.Console.WriteLine("Output:");
+            //System.Console.Write(string.Join(Environment.NewLine, output));
 
-            System.Console.ReadLine();
+            //System.Console.ReadLine();
         }
     }
 }
---
+++ CodingProblems/CodingProblems/takeuforward/CountDigitsInNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.takeuforward
+{
+    // https://takeuforward.org/data-structure/count-digits-in-a-number/
+    public static class CountDigitsInNumber
+    {
+        public static int CountDigits(int number)
+        {
+            int count = 0;
+            while (number / 10 > 0) 
+            {
+                count++;
+                number = number / 10;
+            }
+            if (number < 10)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public static int CountDigitsImproved(int number)
+        {
+            int count = 0;
+            while (number  > 0)
+            {
+                count++;
+                number = number / 10;
+            }
+            return count;
+        }
+
+        public static int CountDigitsOptimized(int number)
+        {
+
+            int count = Convert.ToInt32(Math.Log10(number) + 1);
+            return count;
+        }
+    }
+}
