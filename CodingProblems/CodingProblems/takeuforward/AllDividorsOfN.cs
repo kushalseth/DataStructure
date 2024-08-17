6f2eb1ef83fcd65c92d44ed65761758d07a7bbce
Kushal Seth <kushalseth1408@gmail.com>
2024-08-17 15:56:38 +0200

    all divisors

---
+++ CodingProblems/CodingProblems/takeuforward/AllDividorsOfN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems.takeuforward
+{
+    public static class AllDividorsOfN
+    {
+        public static int[] Calculate(int number)
+        {
+            int[] result;
+
+
+            return null;
+        }
+    }
+}
